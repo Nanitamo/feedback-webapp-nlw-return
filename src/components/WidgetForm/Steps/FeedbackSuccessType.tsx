0f859e33fdb2abcfac4419bd832fc85f7601df6c
Nanitamo <nanitamo19@gmail.com>
2022-05-06 20:18:26 +0100

    Finished Front-end

--- src/components/WidgetForm/Steps/FeedbackSuccessType.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessType.tsx
@@ -1,6 +1,32 @@
-export function FeedbackSuccessStep() {
-    return(
+import { CloseButton } from "../../CloseButton";
 
-        <h1></h1>
+import successImage from '../../../assets/success.svg';
+
+interface FeedbackSuccessTypePros{
+    onFeedbackRestartRequested: () =>void
+}
+export function FeedbackSuccessStep({
+    onFeedbackRestartRequested
+}: FeedbackSuccessTypePros) {
+    return (
+        <>
+            <header>
+                <CloseButton />
+            </header>
+
+            <div className="flex flex-col items-center py-10 w-[304px]">
+                <img src={successImage} alt="Imagem de Sucesso" width={40} height={40} />
+
+                <span className="text-xl mt-2">Agradecemos o feddback!</span>
+
+                <button
+                    type="button"
+                    onClick={onFeedbackRestartRequested}
+                    className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm hover:bg-zinc-700 transition-colors focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none"
+                >
+                    Quero enviar outro
+                </button>
+            </div>
+        </>
     )
 }
